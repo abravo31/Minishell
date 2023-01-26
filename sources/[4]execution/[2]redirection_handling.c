6cb8d8bd03bc42e3442f4f66cd9b69e0149900ef
motero <motero@student.42.fr>
2023-01-26 23:40:35 +0100

    redirectiosn work, now let's try to do heredoc so can  handle the true rediretion

--- sources/[4]execution/[2]redirection_handling.c
+++ sources/[4]execution/[2]redirection_handling.c
@@ -6,7 +6,7 @@
 /*   By: motero <motero@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 22:26:31 by motero            #+#    #+#             */
-/*   Updated: 2023/01/26 23:09:36 by motero           ###   ########.fr       */
+/*   Updated: 2023/01/26 23:39:31 by motero           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,59 +15,59 @@
 void	redirect_output(t_minishell *msh, t_ast *root)
 {
 	int		fd;
-	t_ast	*left;
 	t_ast	*right;
 
-	fd = open(right->data, O_RDONLY, 0644);
-	if (fd == -1)
-	{
-		printf("Error: %s\n", strerror(errno));
-		return ;
-	}
-	ft_lstadd_front(msh.fd, ft_lstnew(fd));
-}
-
-void	redirect_input(t_minishell *msh, t_ast *root)
-{
-	int		fd;
-	t_ast	*left;
-	t_ast	*right;
-
+	right = root->right;
 	fd = open(right->data, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (fd == -1)
 	{
 		printf("Error: %s\n", strerror(errno));
 		return ;
 	}
-	ft_lstadd_front(msh.fd, ft_lstnew(fd));
+	ft_lstadd_front(&msh->fd, ft_lstnew(&fd));
 }
 
-void	redirect_append(t_minishell *msh, t_ast *root)
+void	redirect_input(t_minishell *msh, t_ast *root)
 {
 	int		fd;
-	t_ast	*left;
 	t_ast	*right;
 
-	fd = open(right->data, O_WRONLY | O_CREAT | O_APPEND, 0644);
-	if (fd == -1)
-	{
-		printf("Error: %s\n", strerror(errno));
-		return ;
-	}
-	ft_lstadd_front(msh.fd, ft_lstnew(fd));
-}
-
-void	redirect_heredoc(t_minishell *msh, t_ast *root)
-{
-	int		fd;
-	t_ast	*left;
-	t_ast	*right;
-
+	right = root->right;
 	fd = open(right->data, O_RDONLY, 0644);
 	if (fd == -1)
 	{
 		printf("Error: %s\n", strerror(errno));
 		return ;
 	}
-	ft_lstadd_front(msh.fd, ft_lstnew(fd));
+	ft_lstadd_front(&msh->fd, ft_lstnew(&fd));
+}
+
+void	redirect_append(t_minishell *msh, t_ast *root)
+{
+	int		fd;
+	t_ast	*right;
+
+	right = root->right;
+	fd = open(right->data, O_WRONLY | O_CREAT | O_APPEND, 0644);
+	if (fd == -1)
+	{
+		printf("Error: %s\n", strerror(errno));
+		return ;
+	}
+	ft_lstadd_front(&msh->fd, ft_lstnew(&fd));
+}
+
+void	redirect_heredoc(t_minishell *msh, t_ast *root)
+{
+	int		fd;
+	t_ast	*right;
+
+	right = root->right;
+	fd = open(right->data, O_RDONLY, 0644);
+	if (fd == -1)
+	{
+		printf("Error: %s\n", strerror(errno));
+		return ;
+	}
+	ft_lstadd_front(&msh->fd, ft_lstnew(&fd));
 }
