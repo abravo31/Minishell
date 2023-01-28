846615546f80cb0ebbc3cb762f22ad48d93949e9
motero <motero@student.42.fr>
2023-01-28 22:22:08 +0100

    garbage collector files started

---
+++ includes/garbage_collector.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtin.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: motero <motero@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/08 11:36:43 by motero            #+#    #+#             */
+/*   Updated: 2023/01/07 17:04:29 by motero           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GARBAGE_COLLECTOR_H
+# define GARBAGE_COLLECTOR_H
+
+# include "libft.h"
+# include "structures.h"
+
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+/*############################################################################*/
+/*                            GARBAGE COLLECTOR FUNCTIONS                     */
+/*############################################################################*/
+
+
+#endif
