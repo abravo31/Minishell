f24badad4037428505a6980a2c1029c20f662eb5
motero <motero@student.42.fr>
2023-02-03 17:55:10 +0100

    fixed echo

--- sources/[3]builtin/[4]echo.c
+++ sources/[3]builtin/[4]echo.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   [3]ft_miscellaneous.c                              :+:      :+:    :+:   */
+/*   [4]echo.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: motero <motero@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 20:33:00 by motero            #+#    #+#             */
-/*   Updated: 2023/01/11 14:27:53 by motero           ###   ########.fr       */
+/*   Updated: 2023/02/03 17:52:18 by motero           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,17 +24,16 @@ int	builtin_echo(t_minishell *msh, t_ast *root)
 		return (0);
 	flag = 0;
 	i = 1;
-	while (cmd[i] != NULL)
+	while (cmd[i] != NULL && is_flag(cmd[i]))
 	{
-		if (is_flag(cmd[i]) == 0)
-			break ;
 		flag = 1;
 		i++;
 	}
 	while (cmd[i] != NULL)
 	{	
 		ft_putstr_fd(cmd[i++], 1);
-		ft_putstr_fd(" ", 1);
+		if (cmd[i] != NULL)
+			ft_putstr_fd(" ", 1);
 	}
 	if (flag == 0)
 		ft_putstr_fd("\n", 1);
