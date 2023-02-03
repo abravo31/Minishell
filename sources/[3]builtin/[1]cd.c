a6ab6a7e670d81fdfe1f6f878a53b9b139742a90
motero <motero@student.42.fr>
2023-02-03 23:57:05 +0100

    created other fils now starting with cd

--- sources/[3]builtin/[1]cd.c
+++ sources/[3]builtin/[1]cd.c
@@ -1,36 +1,22 @@
-
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   [2]ft_environment.c                                :+:      :+:    :+:   */
+/*   [1]cd.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: motero <motero@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 20:33:00 by motero            #+#    #+#             */
-/*   Updated: 2023/01/09 23:11:51 by motero           ###   ########.fr       */
+/*   Updated: 2023/02/03 23:56:41 by motero           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtin.h"
 #include "structures.h"
 
-int	builtin_export(t_minishell *msh, t_ast *root)
+int	builtin_cd(t_minishell *msh, t_ast *root)
 {
 	(void)msh;
 	(void)root;
-	return (printf("Execute export\n"));
+	return (printf("Execute cd\n"));
 }
 
-int	builtin_unset(t_minishell *msh, t_ast *root)
-{
-	(void)msh;
-	(void)root;
-	return (printf("Execute unset\n"));
-}
-
-int	builtin_env(t_minishell *msh, t_ast *root)
-{
-	(void)msh;
-	(void)root;
-	return (printf("Execute env\n"));
-}
--- sources/[3]builtin/[5]pwd.c
+++ sources/[3]builtin/[5]pwd.c
@@ -1,24 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   [1]ft_directory.c                                  :+:      :+:    :+:   */
+/*   [5]pwd.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: motero <motero@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 20:33:00 by motero            #+#    #+#             */
-/*   Updated: 2023/01/09 23:11:12 by motero           ###   ########.fr       */
+/*   Updated: 2023/02/03 23:56:35 by motero           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtin.h"
-#include "structures.h"
-
-int	builtin_cd(t_minishell *msh, t_ast *root)
-{
-	(void)msh;
-	(void)root;
-	return (printf("Execute cd\n"));
-}
 
 int	builtin_pwd(t_minishell *msh, t_ast *root)
 {
